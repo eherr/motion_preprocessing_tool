923f76c2b089da53c4075c6cce4eea19cdba6bc2
erik <erik.herrmann@hotmail.com>
2020-03-20 22:05:36 +0100

    Added function to mirror the joint map and functions to rotate the axes to the SkeletonEditorDialog

--- motion_analysis/gui/dialogs/skeleton_editor_dialog.py
+++ motion_analysis/gui/dialogs/skeleton_editor_dialog.py
@@ -27,6 +27,7 @@ from copy import copy
 from PySide2.QtWidgets import  QDialog, QListWidgetItem, QTableWidgetItem, QTableWidget, QFileDialog
 from PySide2.QtCore import QTimer, Qt
 from PySide2.QtGui import QColor
+from OpenGL.GL import *
 from motion_analysis.gui.layout.skeleton_editor_dialog_ui import Ui_Dialog
 from .utils import get_animation_controllers
 from transformations import quaternion_matrix, quaternion_multiply, quaternion_about_axis
@@ -37,7 +38,7 @@ from vis_utils.animation.animation_editor import AnimationEditor
 from vis_utils.io import save_json_file
 from anim_utils.animation_data import BVHReader, BVHWriter, MotionVector, SkeletonBuilder
 from anim_utils.retargeting.analytical import create_local_cos_map_from_skeleton_axes_with_map, find_rotation_between_vectors, OPENGL_UP_AXIS
-from OpenGL.GL import *
+from anim_utils.animation_data.skeleton_models import STANDARD_MIRROR_MAP, STANDARD_MIRROR_MAP_LEFT, STANDARD_MIRROR_MAP_RIGHT, JOINT_CONSTRAINTS
 
 
 
@@ -126,8 +127,104 @@ def get_axis_correction(skeleton, joint_name, up_vector, target_vector=OPENGL_UP
     return qoffset
 
 
+
+def get_child_index(skeleton, joint_name):
+    for idx, c in enumerate(skeleton.nodes[joint_name].parent.children):
+        if c.node_name == joint_name:
+            return idx 
+    return -1
+
+def get_traversal_map_from_parent(skeleton, joint_name, target_parent):
+    traversal_map = []
+    while joint_name != target_parent and skeleton.nodes[joint_name].parent is not None:
+        #print("get traversal map", joint_name, target_parent, )
+        idx = get_child_index(skeleton, joint_name)
+        if idx == -1:
+            print("Error index is none", joint_name, skeleton.nodes[joint_name].parent.children)
+            break
+        traversal_map.append(idx)
+        joint_name = skeleton.nodes[joint_name].parent.node_name
+    return list(reversed(traversal_map))
+
+def get_joint_from_traversal_map(skeleton, traversal_map, joint_name):
+    for idx in traversal_map:
+        joint_name = skeleton.nodes[joint_name].children[idx].node_name
+    return joint_name
+
+
+def generate_sequence_from_root_to_joint(skeleton, skeleton_model, joint_key_list, joint_val_list, joint_name, known_joints, partial_mirror_map):
+    joint_list = []
+    while joint_name != "pelvis":
+        #print("add", joint_name)
+        joint_list.append(joint_name)
+        skel_j = skeleton_model["joints"][joint_name]
+        parent = skeleton.nodes[skel_j].parent
+        while parent is not None and parent.node_name not in joint_val_list:
+            parent = parent.parent
+        if parent is None:
+            print("Error: parent is None")
+            break
+        idx = joint_val_list.index(parent.node_name)
+        joint_name = joint_key_list[idx]
+        skel_j = skeleton_model["joints"][joint_name]
+        if (len(joint_list)> 1 and joint_name in known_joints) or  joint_name not in partial_mirror_map:
+            break
+    return list(reversed(joint_list))
+
+def mirror_sequence(skeleton, src_joint_list, skeleton_model):
+    """ mirror sequence by corresponding joint hierarchy"""
+    standard_mirror_map = STANDARD_MIRROR_MAP 
+    joint_map = skeleton_model["joints"]
+    parent_src = src_joint_list[0]
+    parent_dst = standard_mirror_map[parent_src]
+    if parent_dst not in joint_map:
+        return skeleton_model
+    for src in src_joint_list[1:]:
+        src_j = joint_map[src]
+        dst = standard_mirror_map[src]
+        src_parent_j = joint_map[parent_src]
+        parent_dst_j = joint_map[parent_dst]
+        traversal_map = get_traversal_map_from_parent(skeleton, src_j, src_parent_j)
+        dst_j = get_joint_from_traversal_map(skeleton, traversal_map, parent_dst_j)
+        skeleton_model["joints"][dst] = dst_j
+        print("set", dst, dst_j)
+        #src_cos_map = skeleton_model["cost_map"][src_j]
+        #skeleton_model["cost_map"][dst_j] = src_cos_map
+        parent_src = src
+        parent_dst = dst
+
+    return skeleton_model
+
+def mirror_join_map(skeleton, skeleton_model, partial_mirror_map):
+    """ for each end effector generate a sequence to the root or the last mirrored joint and then try to mirror the sequence"""
+    known_joints = set()
+    joint_key_list = list(skeleton_model["joints"].keys()) 
+    joint_val_list = list(skeleton_model["joints"].values()) 
+    for key in partial_mirror_map:
+        print("try to mirror  ", key)
+        if key in known_joints:
+            print("seen joint already")
+            continue
+        if key not in skeleton_model["joints"] :
+            print(key, "not in joint map")
+            continue
+        if skeleton_model["joints"][key] is None:
+            print("joint map set to None")
+            continue
+        #skel_j = skeleton_model["joints"][key]
+        #if len(skeleton.nodes[skel_j].children) > 0:
+        #    print("too many children")
+        #    continue
+        sequence = generate_sequence_from_root_to_joint(skeleton, skeleton_model, joint_key_list, joint_val_list, key, known_joints, partial_mirror_map)
+        print("generate ", sequence)
+        for k in sequence:
+            known_joints.add(k)
+        skeleton_model = mirror_sequence(skeleton, sequence, skeleton_model)
+    return skeleton_model
+    
 class SkeletonEditorDialog(QDialog, Ui_Dialog):
     def __init__(self, name, skeleton, share_widget, parent=None, enable_line_edit=False, skeleton_model=None):
+        self.initialized = False
         QDialog.__init__(self, parent)
         Ui_Dialog.setupUi(self, self)
         self.view = SceneViewerWidget(parent, share_widget, size=(400,400))
@@ -186,6 +283,8 @@ class SkeletonEditorDialog(QDialog, Ui_Dialog):
 
         self.setOrthogonalTwistButton.clicked.connect(self.slot_set_orthogonal_twist)
         self.setOrthogonalSwingButton.clicked.connect(self.slot_set_orthogonal_swing)
+        self.rotateTwistButton.clicked.connect(self.slot_rotate_twist)
+        self.rotateSwingButton.clicked.connect(self.slot_rotate_swing)
 
         self.flipTwistButton.clicked.connect(self.slot_flip_twist)
         self.flipSwingButton.clicked.connect(self.slot_flip_swing)
@@ -195,10 +294,15 @@ class SkeletonEditorDialog(QDialog, Ui_Dialog):
         self.alignToForwardAxisButton.clicked.connect(self.slot_align_to_forward_axis)
 
         self.setGuessButton.clicked.connect(self.slot_guess_cos_map)
+        self.resetCosButton.clicked.connect(self.slot_reset_cos_map)
         self.loadDefaultPoseButton.clicked.connect(self.slot_load_default_pose)
         self.applyScaleButton.clicked.connect(self.slot_apply_scale)
         self.jointMapComboBox.currentIndexChanged.connect(self.slot_update_joint_map)
         self.aligningRootComboBox.currentIndexChanged.connect(self.slot_update_aligning_root_joint)
+
+        self.mirrorLeftButton.clicked.connect(self.slot_mirror_left_to_right)
+        self.mirrorRightButton.clicked.connect(self.slot_mirror_right_to_left)
+
         self.is_updating_joint_info = False
         self.success = False
         self.initialized = False
@@ -219,12 +323,10 @@ class SkeletonEditorDialog(QDialog, Ui_Dialog):
             if index >= 0:
                 self.aligningRootComboBox.setCurrentIndex(index)
 
-
     def closeEvent(self, e):
          self.timer.stop()
          self.view.makeCurrent()
          del self.view
-
 
     def on_mouse_click(self, event, ray_start, ray_dir, pos, node_id):
         if event.button() == Qt.LeftButton:
@@ -265,6 +367,8 @@ class SkeletonEditorDialog(QDialog, Ui_Dialog):
             g_twist = normalize(g_twist)
             q = axes_to_q(g_twist, g_swing)
             m = quaternion_matrix(q)
+            #print("g_twist", g_twist, twist)
+            #print("g_swing", g_swing, swing)
             self.scene.scene_edit_widget.rotation = m[:3,:3].T
         else:
             print("no cos map", self.skeleton_model.keys())
@@ -285,6 +389,7 @@ class SkeletonEditorDialog(QDialog, Ui_Dialog):
     def fill_joint_map(self):
         self.jointMapComboBox.clear()
         for idx, joint in enumerate(STANDARD_JOINTS):
+            print("add", joint)
             self.jointMapComboBox.addItem(joint, idx)
 
     def fill_root_combobox(self):
@@ -459,6 +564,42 @@ class SkeletonEditorDialog(QDialog, Ui_Dialog):
         self.set_swing_text(swing)
         self.update_joint_info(joint_knob)
 
+    def slot_rotate_twist(self):
+        joint_knob = self.get_selected_joint()
+        if joint_knob is None:
+            return
+        joint_name = joint_knob.joint_name
+        #get twist axis 
+        angle = round(float(self.twistRotationLineEdit.text()),self.precision)
+        rotation_axis = np.array(self.skeleton_model["cos_map"][joint_name]["x"])
+        q = quaternion_about_axis(np.deg2rad(angle), rotation_axis)
+        q = normalize(q)
+        twist = np.array(self.skeleton_model["cos_map"][joint_name]["y"])
+        twist = rotate_vector(q, twist)
+        twist = normalize(twist)
+        self.skeleton_model["cos_map"][joint_name]["y"] = twist
+        self.set_twist_text(twist)
+        self.update_joint_info(joint_knob)
+
+    def slot_rotate_swing(self):
+        joint_knob = self.get_selected_joint()
+        if joint_knob is None:
+            return
+        joint_name = joint_knob.joint_name
+        
+        angle = round(float(self.swingRotationLineEdit.text()),self.precision)
+        rotation_axis = np.array(self.skeleton_model["cos_map"][joint_name]["y"])
+        q = quaternion_about_axis(np.deg2rad(angle), rotation_axis)
+        q = normalize(q)
+        swing = np.array(self.skeleton_model["cos_map"][joint_name]["x"])
+        swing = rotate_vector(q, swing)
+        swing = normalize(swing)
+
+        self.skeleton_model["cos_map"][joint_name]["x"] = swing
+        self.set_swing_text(swing)
+        self.update_joint_info(joint_knob)
+
+
     def slot_flip_z_axis(self):
         joint_knob = self.get_selected_joint()
         if joint_knob is None:
@@ -488,6 +629,30 @@ class SkeletonEditorDialog(QDialog, Ui_Dialog):
         cos_map[joint_knob.joint_name] = cos_map_copy[joint_knob.joint_name]
         self.skeleton_model["cos_map"] = cos_map
         self.update_joint_info(joint_knob)
+
+    def slot_reset_cos_map(self):
+        joint_knob = self.get_selected_joint()
+        if joint_knob is None:
+            return
+        joint_name = joint_knob.joint_name
+        if joint_name in self.skeleton_model["cos_map"]:
+            up_vector = self.skeleton_model["cos_map"][joint_name]["y"]
+            target_vector = [1,0,0]
+            m = self.skeleton.nodes[joint_name].get_global_matrix(self.skeleton.reference_frame)[:3,:3]
+            m_inv = np.linalg.inv(m)
+            target_vector = normalize(target_vector)
+            local_target = np.dot(m_inv, target_vector)
+            local_target = normalize(local_target)
+            self.skeleton_model["cos_map"][joint_name]["y"] = local_target
+
+            x_vector = self.skeleton_model["cos_map"][joint_name]["x"]
+            q = quaternion_from_vector_to_vector(up_vector, local_target)
+            x_vector = rotate_vector(q, x_vector)
+            
+            x_vector -= x_vector.dot(local_target) * local_target      # make it orthogonal to twist
+            x_vector /= np.linalg.norm(x_vector)  # normalize it
+            self.skeleton_model["cos_map"][joint_name]["x"] = normalize(x_vector)
+            self.update_joint_info(joint_knob)
 
     
     def slot_update_joint_map(self):
@@ -572,3 +737,11 @@ class SkeletonEditorDialog(QDialog, Ui_Dialog):
             self.skeleton_model["cos_map"][joint_name]["x"] = normalize(x_vector)
             self.update_joint_info(joint_knob)
 
+    def slot_mirror_left_to_right(self):
+        self.skeleton_model = mirror_join_map(self.skeleton, self.skeleton_model, STANDARD_MIRROR_MAP_LEFT)
+        print("mirrored left to right") 
+        print(self.skeleton_model["joints"])
+
+    def slot_mirror_right_to_left(self):
+        self.skeleton_model = mirror_join_map(self.skeleton, self.skeleton_model, STANDARD_MIRROR_MAP_RIGHT)
+        print("mirrored right to left") 
