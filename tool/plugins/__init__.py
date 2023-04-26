35ed7d3eadacc11bc3b61d191b420ff955c1717d
Erik Herrmann <erik.herrmann@dfki.de>
2023-04-26 19:51:24 +0200

    Changed: ignore exceptions when importing plugins

--- tool/plugins/__init__.py
+++ tool/plugins/__init__.py
@@ -1,7 +1,10 @@
 import importlib
 from pathlib import Path
-
 for plugin_dir in Path(__file__).parent.iterdir():
     if plugin_dir.joinpath("__init__.py").is_file():
-        print("import", plugin_dir.stem, "plugin")
-        importlib.import_module("tool.plugins."+str(plugin_dir.stem))
+        try:
+            print("Importing "+ plugin_dir.stem + " plugin...")
+            importlib.import_module("tool.plugins."+str(plugin_dir.stem))
+        except Exception as e:
+            print("Error: Could not import plugin "+plugin_dir.stem + ": "+str(e))
+            pass
